584d9c513d0dfe32ae74b9e9d9e4bce42356639a
Magesh <tkmagesh77@gmail.com>
2019-04-23 10:40:43 +0530

    "describe" api implementation and sort assignment

--- 03-fp/products.js
+++ 03-fp/products.js
@@ -6,38 +6,44 @@ var products = [
 	{id : 1, name : 'Ken', cost : 20, units : 80, category : 'utencil'},
 ];
 
-console.group('Default List');
-console.table(products);
-console.groupEnd();
+function describe(title, fn){
+	console.group(title);
+	fn();
+	console.groupEnd();
+}
 
-console.group('Sort');
-console.group('Default sort [products by id]');
-function sort(){};
-sort();
-console.table(products);
-console.groupEnd();
+describe('Default List', function(){
+	console.table(products);
+});
 
-console.group('Any list by any attribute');
-function sort(){};
-console.group('products by cost');
-sort();
-console.table(products);
-console.groupEnd();
+describe('Sort', function(){
+	describe('Default sort [products by id]', function(){
+		function sort(){};
+		sort();
+		console.table(products);
+	});
 
-console.group('products by units');
-sort();
-console.table(products);
-console.groupEnd();
+	describe('Any list by any attribute', function(){
+		function sort(/*....*/){};
+		describe('products by cost', function(){
+			sort();
+			console.table(products);
+		});
 
-console.groupEnd();
-console.groupEnd();
+		describe('products by units', function(){
+			sort();
+			console.table(products);
+		});
 
-console.group('Filter');
-console.group('Default filter [stationary products]');
-function filter(){}
-filter();
-console.table(products);
-console.groupEnd();
-console.groupEnd();
+	});
+});
+
+/*describe('Filter', function(){
+	describe('Default filter [stationary products]', function(){
+		function filter(){}
+		filter();
+		console.table(products);
+	});
+});*/
 
 
